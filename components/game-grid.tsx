5defa82e85dbbc8cdfaecad24e89a4e1714a7520
stew.chan@gmail.com <stew.chan@gmail.com>
2025-12-03 15:05:55 -0500

    Shrink cards: tighter grid

--- components/game-grid.tsx
+++ components/game-grid.tsx
@@ -11,7 +11,7 @@ export default function GameGrid({ games }: Props) {
 
   return (
     <section id="games" className="space-y-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
         {sorted.map((game) => (
           <Link
             key={game.slug}
